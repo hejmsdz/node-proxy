a5d979184afabaf9d250e5313d7eccd19dfa57f1
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-07 21:38:53 +0100

    Primera versión de un proxy sencillo

--- src/ProxyServer.js
+++ src/ProxyServer.js
@@ -1,10 +1,25 @@
 const http = require('http');
+const { URL } = require('url');
 
 const ProxyServer = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(`URL: ${req.url}\n`);
-  res.write(`Headers: ${JSON.stringify(req.headers)}`);
-  res.end();
+  const url = new URL(req.url);
+  const options = {
+    hostname: url.hostname,
+    port: 80,
+    path: url.pathname,
+    method: 'GET'
+  };
+  console.log(url.href);
+
+  const proxy = http.request(options, function (proxyRes) {
+    proxyRes.pipe(res, {
+      end: true
+    });
+  });
+
+  req.pipe(proxy, {
+    end: true
+  });
 });
 
 module.exports = ProxyServer;
