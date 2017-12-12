fa1cae4a7159aa302493bcf5d7e3d9177c7c15fb
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-12 18:56:27 +0100

    Un protótipo del proxy inverso, no sé para qué

--- src/ProxyServer.js
+++ src/ProxyServer.js
@@ -1,16 +1,36 @@
 const http = require('http');
 const { URL } = require('url');
 
+const reverseProxyTable = {
+  'horario': 'localhost:3000',
+  'personal': 'localhost:4000',
+};
+
 const ProxyServer = http.createServer((req, res) => {
-  const url = new URL(req.url);
+  //const url = new URL(req.url);
+
+  let pathParts = req.url.split('/');
+  console.log(pathParts);
+  let app = pathParts[1];
+  if (pathParts.length < 1 || !(app in reverseProxyTable)) {
+    res.writeHead(400);
+    res.end('error');
+    return;
+  }
+  let path = pathParts.slice(2).join('/');
+
+
+  let target = reverseProxyTable[app];
+  let [targetHost, targetPort] = target.split(':');
+
   const options = {
-    hostname: url.hostname,
-    port: url.port,
-    path: url.pathname + url.search,
+    hostname: targetHost,
+    port: targetPort,
+    path: path,
     method: req.method,
     headers: req.headers
   };
-  console.log(url.href);
+  console.log(req.url);
 
   const proxy = http.request(options, function (proxyRes) {
     res.writeHead(proxyRes.statusCode, proxyRes.headers);
