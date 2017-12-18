c96a6bb22afbc55808a146e1fcd95280cd610195
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-18 11:37:56 +0100

    Comprobación sencilla de la cacheabilidad

--- src/cache/IsCacheable.js
+++ src/cache/IsCacheable.js
@@ -1,9 +1,22 @@
 /**
  * Determinar si una respuesta HTTP se puede almacenar en la caché.
- * 
- * Obviamente, hay que desarollar este código, pasando también parámetros adecuados.
  */
-function IsCacheable() {
+function IsCacheable(req, res) {
+  const cacheControl = (res.headers['cache-control'] || '').split(', ');
+
+  // por simplicidad sólo respuestas 200 OK y el método GET
+  if (res.statusCode != 200) {
+    return false;
+  }
+
+  if (res.method != 'GET') {
+    return false;
+  }
+
+  if (cacheControl.find(order => order == 'private' || order == 'no-store')) {
+    return false;
+  }
+
   return true;
 }
 
